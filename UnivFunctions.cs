a99d0a4d668ec95d7a1c03b068d748ac381b72d7
jluckymissychristian@gmail.com <jluckymissychristian@gmail.com>
2025-04-01 11:40:26 -0500

    Add project files.

---
+++ UnivFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal_project_CIS_131;
+
+class UnivFunctions
+{
+    public static bool InvertBool(bool input)
+    {return !input;}
+
+    public static int InvertInt(int input) 
+    {return input * -1;}
+
+
+}
