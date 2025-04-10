b7737c22481d7ee0a0ed3959e66a273fc4b13880
Kaleb Hilla <kaleb.hilla@southeasttech.edu>
2025-04-10 10:24:56 -0500

    stuff

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 /*
 Joshua Whitmore
@@ -10,9 +11,12 @@ namespace Assembly_emulator
 {
     public partial class AssemblyEmulator : Form
     {
+        private ExecuteCommand executecommand;
+
         public AssemblyEmulator()
         {
             InitializeComponent();
+            executecommand = new ExecuteCommand(OutputRTB);
         }
         Registers registers = new Registers();
 
@@ -30,6 +34,39 @@ namespace Assembly_emulator
 
         public void PrintRegs()//Josh
         {
+            var userInput = CommandInRTB.Text;
+            ParseCommands(userInput);
+        }
+        private void ParseCommands(string input)
+        {
+            try
+            {
+                var lines = input.Split('\n');
+                foreach (var line in lines)
+                {
+                    var parts = line.Split(' ');
+                    var command = parts[0];
+                    var var1 = int.Parse(parts[1]);
+                    var var2 = int.Parse(parts[2].Substring(1));
+                    var var3 = parts.Length > 3 ? int.Parse(parts[3].Substring(1)) : 0;
+
+                }
+                ExecuteCommands();
+            }
+            catch
+            {
+                MessageBox.Show("Did not work. Please try again.");
+            }
+        }
+        
+        private void ExecuteCommands()
+        {
+            // if you are doing the list please replace UserCommand with the list name
+            foreach (var command in UserCommand)
+            {
+                executecommand.CommandExecute(command, var1, var2, var3);
+            }
+            OutputRTB.AppendText($"R1: {executecommand.Registers.reg1}\nR2: {executecommand.Registers.reg2}\nR3: {executecommand.Registers.reg3}\n");
             //Note: Forms elements DO NOT work outside of the parent forms' classes.
             OutputRTB.Text = ("Register 1 = " + registers.reg1);
             OutputRTB.Text += ("\nRegister 2 = " + registers.reg2);
--- Registers.cs
+++ Registers.cs
@@ -1,10 +1,64 @@
-﻿namespace Assembly_emulator
+﻿using System;
+using System.Windows.Forms;
+
+namespace Assembly_emulator
 {
-    internal class Registers //Josh
+    public class Registers
     {
         public int reg1 = 0;
         public int reg2 = 0;
         public int reg3 = 0;
+        private RichTextBox outputRTB;
+        public Registers(RichTextBox outputRTB)
+        {
+            reg1 = 0;
+            reg2 = 0;
+            reg3 = 0;
+            this.outputRTB = outputRTB;
+        }
+        public void ExecuteLD(int value, int register)
+        {
+            if (register == 1)
+                reg1 = value;
+            else if (register == 2)
+                reg2 = value;
+            else if (register == 3)
+                reg3 = value;
+            outputRTB.AppendText($"LD {value} R{register}\n");
 
+        }
+        public void ExecuteADD(int reg1, int reg2, int reg3)
+        {
+            this.reg3 = this.reg1 + this.reg2;
+        }
+
+        public void ExecuteSUB(int reg1, int reg2, int reg3)
+        {
+            this.reg3 = this.reg1 - this.reg2;
+        }
+
+        public void ExecuteMUL(int reg1, int reg2, int reg3)
+        {
+            this.reg3 = this.reg1 * this.reg2;
+        }
+
+        public void ExecuteDIV(int reg1, int reg2, int reg3)
+        {
+            if (this.reg2 != 0)
+            {
+                this.reg3 = this.reg1 / this.reg2;
+            }
+            else
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+        }
+        public void ExecuteTRP(int code)
+        {
+            if (code == 3)
+            {
+                outputRTB.AppendText($"R3: {this.reg3}\n");
+            }
+        }
     }
 }
